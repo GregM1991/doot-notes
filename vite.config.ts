f369dae3e23177a75dd3349fc4593b2359e7caa4
GregM1991 <gregm31@live.com>
2024-03-04 05:07:43 +1100

    Add configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
+const MODE = process.env.NODE_ENV;
+
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+	build: {
+		cssMinify: MODE === 'production',
+		rollupOptions: {
+			external: [/node:.*/, 'stream', 'crypto', 'fsevents']
+		}
+	},
+	plugins: [sveltekit()]
 });
