8c54c00b218d3bf00b4c66891b5323b6855a7a1e
Greg <gregm31@live.com>
2024-03-05 06:29:51 +1100

    Adding global styles/fonts

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import '$lib/assets/app.css'
+</script>
+
+<svelte:head>
+  {#if !import.meta.env.Prod}
+    <!-- TODO: Do dev things here if need -->
+  {/if}
+
+  <title>Let's take some notes 📓</title>
+</svelte:head>
+
+<slot />
