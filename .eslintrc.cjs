dda8759e05d4f011dd1dbef1deeb65a118efba68
Greg <gregm31@live.com>
2024-03-07 09:12:44 +1100

    DB up and running with seeded users

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,31 +1,61 @@
 /** @type { import("eslint").Linter.Config } */
+
 module.exports = {
 	root: true,
 	extends: [
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
 		'plugin:svelte/recommended',
-		'prettier'
+		'@remix-run/eslint-config',
+		'@remix-run/eslint-config/node',
+		'prettier',
 	],
 	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint'],
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte']
+		extraFileExtensions: ['.svelte'],
 	},
 	env: {
 		browser: true,
 		es2017: true,
-		node: true
+		node: true,
 	},
 	overrides: [
 		{
 			files: ['*.svelte'],
 			parser: 'svelte-eslint-parser',
 			parserOptions: {
-				parser: '@typescript-eslint/parser'
-			}
-		}
-	]
-};
+				parser: '@typescript-eslint/parser',
+			},
+		},
+	],
+	rules: {
+		'no-empty-pattern': 'off',
+		'@typescript-eslint/consistent-type-imports': [
+			'warn',
+			{
+				prefer: 'type-imports',
+				disallowTypeAnnotations: true,
+				fixStyle: 'inline-type-imports',
+			},
+		],
+		'import/no-duplicates': ['warn', { 'prefer-inline': true }],
+		'import/consistent-type-specifier-style': ['warn', 'prefer-inline'],
+		'import/order': [
+			'warn',
+			{
+				alphabetize: { order: 'asc', caseInsensitive: true },
+				groups: [
+					'builtin',
+					'external',
+					'internal',
+					'parent',
+					'sibling',
+					'index',
+				],
+			},
+		],
+	},
+}
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,47 @@
+/* TODO: will need
+faker,
+promiseHash,
+~~ tests db utils ~~
+createPassword,
+getNoteImages,
+getUserImages,
+img */
+import { prisma } from '$utils/db.server'
+import { createUser, cleanupDb } from '$tests/db-utils'
+
 // create function seed, is main function where all seed logic sits
+async function seed() {
+	console.log('🌱 seeding')
+	console.time('🌱 database has been seeded')
 
-// clean up database
+	console.time('🧹 cleaned up database')
+	await cleanupDb(prisma)
+	console.timeEnd('🧹 cleaned up database')
 
-// call await seed and catch errors, finally call to disconnect prisma
+	// TODO: 🔑 create permissions
+
+	// TODO: 👑 create roles
+
+	const totalUsers = 5
+	console.time(`👫 created ${totalUsers} users...`)
+	for (let i = 0; i < totalUsers; i++) {
+		const userData = createUser()
+		await prisma.user.create({
+			select: { id: true },
+			data: {
+				...userData,
+				// TODO: add password, image, roles, notes
+			},
+		})
+	}
+	console.timeEnd(`👫 created ${totalUsers} users...`)
+	console.timeEnd('🌱 database has been seeded')
+	// TODO: 🐨 create admin user
+}
+
+await seed()
+	.catch(err => {
+		console.error(err)
+		process.exit(1)
+	})
+	.finally(async () => await prisma.$disconnect())
