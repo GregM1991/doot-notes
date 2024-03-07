2933913fccae364091735c0dab7ba79d1e8355a4
Greg <gregm31@live.com>
2024-03-08 08:25:03 +1100

    Hooking up search to an actual search bar

--- src/routes/users/+page.server.ts
+++ src/routes/users/+page.server.ts
@@ -1,7 +1,6 @@
-import type { PageServerLoad } from './$types'
+import type { PageServerLoad, Actions } from './$types'
 import { prisma } from '$utils/db.server'
 import { z } from 'zod'
-import { json } from '@sveltejs/kit'
 
 const UserSearchResultSchema = z.object({
 	id: z.string(),
@@ -12,28 +11,6 @@ const UserSearchResultSchema = z.object({
 const UserSearchResultsSchema = z.array(UserSearchResultSchema)
 
 export const load: PageServerLoad = async ({ request }) => {
-	/* TODO: search functionality
-
-		- get search term from new URL's searchParams
-		- if search term === '' redirect to /users
-
-		- create raw query ($queryRaw) to select user id, username, name and 
-			userimage id from user table
-		- left join the user image on user id
-		- where username is like the search query param
-		- or name is like the search query param
-		- order things by desceding (with inner select ⬇)
-			- select updated at
-			- from notes
-			- where the notes owner id is equal to user id
-			- order by updated at descending
-			- limit 1
-		- limit results to 50
-
-		- safe parse the rawUsers through a user search results schema from zod
-		- if there's no result, return json error with result.error.message and status 400
-		- return json result.data
-	*/
 	const searchQuery = new URL(request.url).searchParams.get('search')
 	const like = `%${searchQuery}%`
 
@@ -51,8 +28,8 @@ export const load: PageServerLoad = async ({ request }) => {
 		) DESC
 		LIMIT 50
 	`
+
 	const result = UserSearchResultsSchema.safeParse(rawUsers)
-	console.log(result)
 	if (!result.success) {
 		return {
 			status: 'error',
--- src/routes/users/+page.svelte
+++ src/routes/users/+page.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
 	import type { PageData } from './$types'
 
+  // TODO: Figure out how to pull the page params
 	export let data: PageData
+  const id = crypto.randomUUID()
 </script>
 
 <h1>User's</h1>
 <div>
+	<form method="GET">
+		<label class="sr-only" for={id}> Search </label>
+		<input placeholder="Search" name="search" type="search" id={id} />
+		<button>Search</button>
+	</form>
 	{#if data.status === 'error'}
 		<span>{data.error}</span>
 	{:else if data.users.length}
