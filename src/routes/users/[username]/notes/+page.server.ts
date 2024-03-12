b369faae8ab683ef402bb32e93c8cf520a39abc7
GregM1991 <gregm31@live.com>
2024-03-13 04:40:26 +1100

    WIP

--- src/routes/users/[username]/notes/+page.server.ts
+++ src/routes/users/[username]/notes/+page.server.ts
@@ -1,6 +1,14 @@
+import { prisma } from '$lib/utils/db.server'
 import type { PageServerLoad } from './$types'
 
 export const load = (async ({ parent }) => {
 	const { user } = await parent()
+
+	const userNotes = await prisma.note.findMany({
+		select: { id: true },
+		where: {
+			ownerId: user.id,
+		},
+	})
 	return { user }
 }) satisfies PageServerLoad
