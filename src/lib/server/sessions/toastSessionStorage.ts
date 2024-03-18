e0498f12d9f78d68cd090a4f22c858c504d2e320
GregM1991 <gregm31@live.com>
2024-03-19 05:10:03 +1100

    Woring on toast cookies

--- src/lib/server/sessions/toastSessionStorage.ts
+++ src/lib/server/sessions/toastSessionStorage.ts
@@ -1,13 +1,25 @@
+import { createId as cuid } from '@paralleldrive/cuid2'
 import { dev } from '$app/environment'
 import type { CookieSerializeOptions } from 'cookie'
 import { SESSION_SECRET } from '$env/static/private'
+import { z } from 'zod'
+
+const ToastSchema = z.object({
+	description: z.string(),
+	id: z.string().default(() => cuid()),
+	title: z.string().optional(),
+	type: z.enum(['message', 'success', 'error']).default('message'),
+})
+
+export type Toast = z.infer<typeof ToastSchema>
+export type ToastInput = z.input<typeof ToastSchema>
 
 interface ICookieSession {
 	name: string
 	options: CookieSerializeOptions & {
 		path?: string
-		secrets?: string[]
 	}
+	secrets?: string[]
 }
 
 export const toastSessionStorage: ICookieSession = {
@@ -17,6 +29,8 @@ export const toastSessionStorage: ICookieSession = {
 		path: '/',
 		httpOnly: true,
 		secure: dev,
-		secrets: SESSION_SECRET.split(','), // TODO: Figure out how to sign our cookies on the server
 	},
+	secrets: SESSION_SECRET.split(','),
 }
+
+export function redirectWithToast(url: string, toast: ToastInput) {}
--- src/routes/users/[username]/notes/[noteid]/+page.server.ts
+++ src/routes/users/[username]/notes/[noteid]/+page.server.ts
@@ -1,12 +1,20 @@
+import { toastSessionStorage } from '$lib/server/sessions/toastSessionStorage'
 import { prisma } from '$lib/utils/db.server'
+import { invariantResponse } from '$lib/utils/misc'
 import { redirect, type Actions } from '@sveltejs/kit'
 
 export const actions = {
-	default: async ({ params }) => {
-		await prisma.note.delete({
+	default: async ({ params, cookies }) => {
+		const note = await prisma.note.findFirst({
+			select: { id: true },
 			where: { id: params.noteid },
 		})
+		invariantResponse(note, 'Not found', 404)
 
-		redirect(303, './')
+		await prisma.note.delete({
+			where: { id: note.id },
+		})
+
+		throw redirect(303, './')
 	},
 } satisfies Actions
