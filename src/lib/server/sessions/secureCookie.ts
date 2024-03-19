37c1962c0ece9f0abdc84882a18b0e7cec24a203
Greg <gregm31@live.com>
2024-03-20 07:39:36 +1100

    Continuing cookie encryption/signing

--- src/lib/server/sessions/secureCookie.ts
+++ src/lib/server/sessions/secureCookie.ts
@@ -1,27 +1,46 @@
 // TODO: encrypt and decrypt function to ensure secure cookies
 
 import crypto from 'crypto'
-import jwt from 'jsonwebtoken'
+import type { ToastInput } from './toastSessionStorage'
+import { SESSION_SECRET } from '$env/static/private'
 
 const algorithm = 'aes-256-cbc'
-const iv = crypto.randomBytes(16)
+const secret = SESSION_SECRET.split(',')[0]
 
-export function encryptCookie(cookieValue: string, secret: string[]) {
-	const cipher = crypto.createCipheriv(
-		algorithm,
-		Buffer.from(secret[0], 'hex'),
-		iv,
-	)
+export function encryptAndSignCookieValue<T>(value: T) {
+	const iv = crypto.randomBytes(16)
+	const cookieValueString = objectToCookieValueString(value)
+	const cipher = crypto.createCipheriv(algorithm, secret, iv)
 
-	const encryptedCookieBuffer = cipher.update(cookieValue)
-	const encryptedCookie = Buffer.concat([
-		encryptedCookieBuffer,
+	const encryptedCookieValueString = cipher.update(cookieValueString)
+	const encryptedCookieValue = Buffer.concat([
+		encryptedCookieValueString,
 		cipher.final(),
 	]).toString('hex')
 
-	return { iv: iv.toString('hex'), encryptedCookie }
+	return objectToCookieValueString({
+		iv: iv.toString('hex'),
+		encryptedCookieValue,
+	})
 }
 
 export function decryptCookie(cookieValue: string, secret: string) {
 	let iv = Buffer.from(cookieValue, 'hex')
 }
+
+function objectToCookieValueString<T>(obj: T): string {
+	const jsonString = JSON.stringify(obj)
+	return encodeURIComponent(jsonString)
+}
+
+function cookieValueStringToObject<T>(value: string): T {
+	const jsonString = decodeURIComponent(value)
+	return JSON.parse(jsonString)
+}
+
+const encryptedCookie = encryptAndSignCookieValue<ToastInput>({
+	description: 'blah',
+	title: 'title',
+	type: 'success',
+})
+console.log({ encryptedCookie })
