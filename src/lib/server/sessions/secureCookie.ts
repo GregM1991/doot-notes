da1f1f10498d27963712cff980a00c5df388cdc2
Greg <gregm31@live.com>
2024-03-20 15:15:19 +1100

    Think I may be there with encryption cookies 👀

--- src/lib/server/sessions/secureCookie.ts
+++ src/lib/server/sessions/secureCookie.ts
@@ -1,14 +1,24 @@
-import crypto from 'crypto'
-import type { ToastInput } from './toastSessionStorage'
+import { fail } from '@sveltejs/kit'
 import { SESSION_SECRET } from '$env/static/private'
+import crypto from 'crypto'
+import jwt from 'jsonwebtoken'
+import { z } from 'zod'
+
+const EncryptedAndSignedCookieSchema = z.object({
+	iv: z.string().length(32),
+	cookieVal: z.string(),
+})
 
 const algorithm = 'aes-256-cbc'
-// const secret = SESSION_SECRET.split(',')[0]
-const secret = '37x9T7RJfZpCBUo6hB2bxd3ZZFj67zNh'
+const secret = SESSION_SECRET.split(',')[0]
 
-export function encryptAndSignCookieValue(value: unknown) {
+export function encryptAndSignCookieValue(
+	value: unknown,
+	signOpts?: Parameters<typeof jwt.sign>[2],
+) {
 	const iv = crypto.randomBytes(16)
 	const cookieValueString = objectToCookieValueString(value)
+	console.log({ cookieValueString })
 	const cipher = crypto.createCipheriv(algorithm, secret, iv)
 
 	const encryptedCookieValueBuffer = cipher.update(cookieValueString)
@@ -16,22 +26,49 @@ export function encryptAndSignCookieValue(value: unknown) {
 		encryptedCookieValueBuffer,
 		cipher.final(),
 	]).toString('hex')
-	const signedandEncryptedCookie = signCookie(encryptedCookieValue)
+	const signedAndEncryptedCookieValue = signCookie(
+		encryptedCookieValue,
+		signOpts,
+	)
 
 	return objectToCookieValueString({
 		iv: iv.toString('hex'),
-		cookieVal: signedandEncryptedCookie,
+		cookieVal: signedAndEncryptedCookieValue,
 	})
 }
 
-export function decryptCookie(cookieValue: string) {
-	const parsedCookieValue = cookieValueStringToObject(cookieValue)
-	console.log(parsedCookieValue)
-	let iv = Buffer.from(cookieValue, 'hex')
+export function decryptCookie(encryptedCookieValue: string) {
+	try {
+		const encryptedAndSignedCookieObj =
+			cookieValueStringToObject(encryptedCookieValue)
+
+		const parsedEncryptedAndSignedCookie =
+			EncryptedAndSignedCookieSchema.safeParse(encryptedAndSignedCookieObj)
+		if (!parsedEncryptedAndSignedCookie.success) {
+			throw new Error('Cookie parse failed')
+		}
+		const { iv, cookieVal } = parsedEncryptedAndSignedCookie.data
+		const verified = jwt.verify(cookieVal, secret)
+		const jwtToken = z.string().safeParse(verified)
+		if (!jwtToken.success) throw new Error('Fuck balls')
+
+		const ivBuffer = Buffer.from(iv, 'hex')
+		const encryptedBuffer = Buffer.from(jwtToken.data, 'hex')
+		const decipher = crypto.createDecipheriv(algorithm, secret, ivBuffer)
+		let decryptedVal = decipher.update(encryptedBuffer, undefined, 'utf8')
+		decryptedVal += decipher.final('utf8')
+
+		return cookieValueStringToObject(decryptedVal)
+	} catch (error) {
+		return fail(403, { error })
+	}
 }
 
-function signCookie(cookieValue: string) {
-	return crypto.createHmac('sha256', secret).update(cookieValue).digest('hex')
+function signCookie(
+	cookieValue: string,
+	opts?: Parameters<typeof jwt.sign>[2],
+) {
+	return jwt.sign(cookieValue, secret, { ...opts })
 }
 
 function objectToCookieValueString<T>(obj: T): string {
@@ -43,15 +80,3 @@ function cookieValueStringToObject(value: string) {
 	const jsonString = decodeURIComponent(value)
 	return JSON.parse(jsonString)
 }
-
-const signedAndEncryptedCookie = encryptAndSignCookieValue({
-	description: 'blah',
-	title: 'title',
-	type: 'success',
-})
-console.log({ signedAndEncryptedCookie })
-console.log(
-	'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~',
-)
-const decryptedCookie = decryptCookie(signedAndEncryptedCookie)
-console.log({ decryptedCookie })
