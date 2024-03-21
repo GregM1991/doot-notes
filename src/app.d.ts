930cab2b60bab22ffd6639c306bcab6461933e85
Greg <gregm31@live.com>
2024-03-21 15:30:26 +1100

    Reverted cookieSessionStorage work

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,14 +1,24 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+import type { Session } from 'svelte-kit-cookie-session'
+
+type SessionData = {
+	views: number
+}
+
 import 'unplugin-icons/types/svelte'
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
+		interface Locals {
+			session: Session<SessionData>
+		}
+		interface PageData {
+			session: SessionData
+		}
 		// interface PageState {}
 		// interface Platform {}
 	}
 }
 
-export {};
+export {}
