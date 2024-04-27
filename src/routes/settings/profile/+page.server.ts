9e75a39ac1d66dc6d41fae05e8a9f1c811c5c4b6
Greg <gregm31@live.com>
2024-04-28 09:30:49 +1000

    Putting in form inputs

--- src/routes/settings/profile/+page.server.ts
+++ src/routes/settings/profile/+page.server.ts
@@ -1,14 +1,25 @@
 import { requireUserId } from '$lib/utils/auth.server'
 import { prisma } from '$lib/utils/db.server'
 import { invariantResponse } from '$lib/utils/misc'
+import { superValidate } from 'sveltekit-superforms'
 import type { PageServerLoad } from './$types'
+import { z } from 'zod'
+import { NameSchema, UsernameSchema } from '$lib/utils/userValidation'
+import { zod } from 'sveltekit-superforms/adapters'
 
-export const load = (async ({ request, locals }) => {
+const ProfileFormSchema = z.object({
+	name: NameSchema.optional(),
+	username: UsernameSchema,
+})
+
+export const load = (async ({ request, locals, parent }) => {
 	const userId = requireUserId(locals.userId, request)
 	const user = await prisma.user.findUnique({
 		where: { id: userId },
 		select: { username: true },
 	})
 	invariantResponse(user, 'User not found', 404)
-	return {}
+	// const {user} = await parent()
+	const editProfileForm = await superValidate(user, zod(ProfileFormSchema))
+	return { editProfileForm }
 }) satisfies PageServerLoad
--- src/routes/settings/profile/+page.svelte
+++ src/routes/settings/profile/+page.svelte
@@ -1,1 +1,28 @@
-<h1>Profile page goes here</h1>
+<script lang="ts">
+	import { Input, Button } from '$lib/components'
+	import { getUserImgSrc } from '$lib/utils/misc'
+	import { superForm } from 'sveltekit-superforms'
+	export let data
+	const { form } = superForm(data.editProfileForm)
+	const profileSrc = getUserImgSrc(data.user.id)
+</script>
+
+<h1 class="header">Let's make some changes to your profile</h1>
+<img class="avatar" src={profileSrc} alt="{data.user?.name}'s avatar" />
+<Input name="username" type="text" bind:value={$form.username} />
+<Input name="name" type="text" bind:value={$form.name} />
+<Button type="submit">Save changes</Button>
+
+<style>
+	.header {
+		font-size: var(--type-step-4);
+		color: var(--palette-pop);
+		text-align: center;
+	}
+
+	.avatar {
+		width: var(--space-3xl);
+		height: var(--space-3xl);
+		object-fit: contain;
+	}
+</style>
