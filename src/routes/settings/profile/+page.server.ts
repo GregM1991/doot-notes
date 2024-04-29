4969490a8bbfc236c3a0f153a498628607c4360b
GregM1991 <gregm31@live.com>
2024-04-29 15:37:00 +1000

    WIP

--- src/routes/settings/profile/+page.server.ts
+++ src/routes/settings/profile/+page.server.ts
@@ -16,10 +16,28 @@ export const load = (async ({ request, locals, parent }) => {
 	const userId = requireUserId(locals.userId, request)
 	const user = await prisma.user.findUnique({
 		where: { id: userId },
-		select: { username: true },
+		select: {
+			id: true,
+			name: true,
+			username: true,
+			email: true,
+			image: {
+				select: { id: true },
+			},
+			// TODO: Bring in session count
+			// _count: {
+			// 	select: {
+			// 		sessions: {
+			// 			where: {
+			// 				expirationDate: { gt: new Date() },
+			// 			},
+			// 		},
+			// 	},
+			// },
+		},
 	})
 	invariantResponse(user, 'User not found', 404)
 	// const {user} = await parent()
 	const editProfileForm = await superValidate(user, zod(ProfileFormSchema))
-	return { editProfileForm }
+	return { editProfileForm, user }
 }) satisfies PageServerLoad
--- src/routes/settings/profile/+page.svelte
+++ src/routes/settings/profile/+page.svelte
@@ -7,13 +7,20 @@
 	const profileSrc = getUserImgSrc(data.user.id)
 </script>
 
-<h1 class="header">Let's make some changes to your profile</h1>
-<img class="avatar" src={profileSrc} alt="{data.user?.name}'s avatar" />
-<Input name="username" type="text" bind:value={$form.username} />
-<Input name="name" type="text" bind:value={$form.name} />
-<Button type="submit">Save changes</Button>
+<div class="wrapper">
+  <h1 class="header">Let's make some changes to your profile</h1>
+  <img class="avatar" src={profileSrc} alt="{data.user?.name}'s avatar" />
+  <Input name="username" type="text" bind:value={$form.username} />
+  <Input name="name" type="text" bind:value={$form.name} />
+  <Button type="submit">Save changes</Button>
+</div>
 
 <style>
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+  }
+
 	.header {
 		font-size: var(--type-step-4);
 		color: var(--palette-pop);
