6db2cea9e75d1cbb42840f67a9c4367418e2f710
Greg <gregm31@live.com>
2024-09-27 12:02:42 +1000

    Redeploy

--- src/lib/utils/misc.ts
+++ src/lib/utils/misc.ts
@@ -60,7 +60,7 @@ export function invariant(
 
 export function safeRedirect(
 	redirectTo: string | null | undefined,
-	path = '/',
+	defaultPath = '/',
 ) {
 	if (
 		redirectTo &&
@@ -69,7 +69,7 @@ export function safeRedirect(
 	) {
 		return redirectTo
 	}
-	return path
+	return defaultPath
 }
 
 /*
